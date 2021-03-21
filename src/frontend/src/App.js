3f70e1411f930262708e17899402d7c06c17f2f1
beom21c <beom21c@gmail.com>
2021-03-21 18:33:37 +0900

    1차 directory 설정

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import {IndexPage} from "./Page/IndexPage";
+import {Header} from "./Component/Header";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+        <IndexPage/>
+        </div>
+    );
+}
+
+export default App;
